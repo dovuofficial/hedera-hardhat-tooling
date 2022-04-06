925309cf23e2c13b6b4c7ed881382141afeedc96
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-04-06 16:00:36 +0100

    Create example-test-contract.js

---
+++ test/stub/example-test-contract.js
@@ -0,0 +1,46 @@
+const { expect } = require("chai");
+
+const {
+  Network,
+  Config,
+  Hashgraph,
+  SDK: {
+    ContractFunctionParameters,
+    AccountId
+  },
+} = require("hashgraph-support")
+
+describe("Testing a contract", function () {
+
+  const destinationNetwork = Config.network
+  const client = Network.getNodeNetworkClient(destinationNetwork)
+  const hashgraph = Hashgraph(client);
+
+  const contractId = process.env.REPLACEME_CONTRACT_ID;
+
+  it("A contract will run a test", async () => {
+
+    // Will run if a contract is Ownable
+    const response = await hashgraph.contract.query({
+      contractId: contractId,
+      method: "owner",
+    })
+
+    const accountId = AccountId.fromSolidityAddress(response.getAddress(0))
+
+    expect(accountId.toString()).to.equal(process.env.HEDERA_ACCOUNT_ID);
+  })
+
+  // it("A contract can call a contract with params ", async () => {
+  //
+  //   const response = await hashgraph.contract.call({
+  //     contractId: contractId,
+  //     method: "a method",
+  //     params: new ContractFunctionParameters()
+  //       .addInt64(2)
+  //   })
+  //
+  //   expect(response).to.be.true;
+  // })
+
+});
