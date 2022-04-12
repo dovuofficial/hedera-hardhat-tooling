0ecae0b238b249333d1ae4808a560902c62dd897
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-04-12 11:17:06 +0100

    Added HelloStruct example with tests

---
+++ contracts/HelloStruct.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// We're just going add open zeppelin, ownable.
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HelloStruct is Ownable {
+
+    // This can be external -> https://solidity-by-example.org/structs
+    struct Hello {
+        string text;
+    }
+
+    Hello public hello;
+
+    constructor() {
+        hello = Hello("hello world");
+    }
+
+    function update(string memory newHello_) public onlyOwner {
+//        hello = Hello(newHello_); // This also works
+        hello.text = newHello_;
+    }
+
+    function getMessage() public view returns(string memory) {
+        return hello.text;
+    }
+}
---
+++ test/hellostruct-contract.js
@@ -0,0 +1,65 @@
+const { expect } = require("chai");
+
+const {
+  Network,
+  Config,
+  Hashgraph,
+  SDK: {
+    ContractFunctionParameters,
+    AccountId
+  },
+} = require("hashgraph-support")
+
+describe("Testing HelloStruct Contract", function () {
+
+  const destinationNetwork = Config.network
+  const client = Network.getNodeNetworkClient(destinationNetwork)
+  const hashgraph = Hashgraph(client);
+
+  const contractId = process.env.HELLOSTRUCT_CONTRACT_ID;
+
+  it("A contract will run a test, ownable", async () => {
+
+    // Will run if a contract is Ownable
+    const response = await hashgraph.contract.query({
+      contractId: contractId,
+      method: "owner",
+    })
+
+    const accountId = AccountId.fromSolidityAddress(response.getAddress(0))
+
+    expect(accountId.toString()).to.equal(process.env.HEDERA_ACCOUNT_ID);
+  })
+
+  it("Will read the basic hello world string", async () => {
+
+    // Will run if a contract is Ownable
+    const response = await hashgraph.contract.query({
+      contractId: contractId,
+      method: "getMessage",
+    })
+
+    expect(response.getString(0)).to.equal("hello world");
+  })
+
+  it("Contract can pass a struct value", async () => {
+
+    const response = await hashgraph.contract.call({
+      contractId: contractId,
+      method: "update",
+      params: new ContractFunctionParameters()
+        .addString('Hello Again')
+    })
+
+    expect(response).to.be.true;
+  })
+
+  it("Will read the updated string", async () => {
+    const response = await hashgraph.contract.query({
+      contractId: contractId,
+      method: "getMessage",
+    })
+
+    expect(response.getString(0)).to.equal("Hello Again");
+  })
+});
